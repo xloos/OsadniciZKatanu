ad66753f7fb65126d169476c9e38efba481f68a2
Filip Loos <xloos@mendelu.cz>
2020-10-08 19:51:40 +0200

    vratil som to naspäť

--- src/main/java/cz/mendelu/pjj/projekt/katan/domain/Game.java
+++ src/main/java/cz/mendelu/pjj/projekt/katan/domain/Game.java
@@ -48,9 +48,6 @@ public class Game {
      * @preturn Metóda vracia true pokial bola ukončená korektne a false pokiaľ sa hráč vzdal alebo sa vyskytla chyba.
      * @author xloos
      * @version 1.0.0
-     *
-     * žeby?
-     * no jo
      */
     public boolean endGame(){
         throw new UnsupportedOperationException("Not implemented yet.");
