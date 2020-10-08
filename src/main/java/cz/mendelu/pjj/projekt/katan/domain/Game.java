f7ce9b944cc6a8119d221b7848d0dcc6e5b78fae
Ondřej Pavlík <xpavlik@mendelu.cz>
2020-10-08 19:54:11 +0200

    zmena

--- src/main/java/cz/mendelu/pjj/projekt/katan/domain/Game.java
+++ src/main/java/cz/mendelu/pjj/projekt/katan/domain/Game.java
@@ -48,6 +48,7 @@ public class Game {
      * @preturn Metóda vracia true pokial bola ukončená korektne a false pokiaľ sa hráč vzdal alebo sa vyskytla chyba.
      * @author xloos
      * @version 1.0.0
+     * nojo
      */
     public boolean endGame(){
         throw new UnsupportedOperationException("Not implemented yet.");
