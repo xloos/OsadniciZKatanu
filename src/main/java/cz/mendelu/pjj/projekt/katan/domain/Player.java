c87bac4b5f5576727846b16c3fc174dca4ee6f52
Ondřej Pavlík <xpavlik@mendelu.cz>
2020-10-09 11:30:05 +0200

    pridana tovarni metoda hrace

--- src/main/java/cz/mendelu/pjj/projekt/katan/domain/Player.java
+++ src/main/java/cz/mendelu/pjj/projekt/katan/domain/Player.java
@@ -1,4 +1,23 @@
 package cz.mendelu.pjj.projekt.katan.domain;
 
 public class Player {
+
+    /**
+     * Tovární metoda na vytvoření hráče.
+     * Metoda vytvoří nového hráče a nastaví atributy resources, buildings a points.
+     * @param name si daný hráč zvolí jméno.
+     * @return Nového hráče.
+     * @author xpavlik
+     * @version 1.0.0
+     */
+
+    public static Player createNewPlayer(String name){
+
+        throw new UnsupportedOperationException("Not implemented yet.");
+
+    }
+
+
+
+
 }
