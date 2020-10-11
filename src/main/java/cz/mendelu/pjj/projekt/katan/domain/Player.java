8731915f12551b18852d86d1f457d547e9e293ab
Ondřej Pavlík <xpavlik@mendelu.cz>
2020-10-11 19:35:48 +0200

    pridana metoda pro stavbu cesty a vesnice

--- src/main/java/cz/mendelu/pjj/projekt/katan/domain/Player.java
+++ src/main/java/cz/mendelu/pjj/projekt/katan/domain/Player.java
@@ -31,6 +31,28 @@ public class Player {
 
     }
 
+    /**
+     * Metoda provede výstavbu nové cesty a zjistí zda má daný hráč dostatek surovin na stavbu.
+     * @return Metoda vrátí true pokud má hráč všechny potřebné suroviny pro stavbu cesty.
+     * @author xpavlik
+     * @version 1.0.0
+     */
+
+    public boolean buildRoad() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+
+    }
+
+    /**
+     * Metoda provede výstavbu nové vesnice, pokud má daný hráč dostatek suroviny na výstavbu.
+     * @return Metoda vrátí true pokud má hráč všechny potřebné suroviny pro stavbu nové vesnice.
+     * @author xpavlik
+     * @version 1.0.0
+     */
+    public boolean buildVillage() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+
+    }
 
 
 
