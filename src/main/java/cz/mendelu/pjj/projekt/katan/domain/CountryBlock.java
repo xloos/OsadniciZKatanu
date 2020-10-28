ff0cbf378d4950cbbc53bbe42fd4e5f3f318ca3f
Filip Loos <xloos@mendelu.cz>
2020-10-28 16:26:56 +0100

    Vytvorenie mapy a mapy map v triede CountryBlock

--- src/main/java/cz/mendelu/pjj/projekt/katan/domain/CountryBlock.java
+++ src/main/java/cz/mendelu/pjj/projekt/katan/domain/CountryBlock.java
@@ -1,15 +1,22 @@
 package cz.mendelu.pjj.projekt.katan.domain;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class CountryBlock {
-   private int type_number;
+
+    Map<Integer,Integer> myblock = new HashMap<Integer, Integer>();
+    List<Map<Integer , Integer>> myMap  = new ArrayList<Map<Integer,Integer>>();
+
+    private int type;
    private int number;
    private boolean pirat;
    private Buildings buildings;
 
     public CountryBlock (int c_type_number, int c_number){
-        this.type_number = c_type_number;
+        this.type = c_type_number;
         this.number = c_number;
-        if (this.type_number == 5){
+        if (this.type == 5){
             this.pirat = true;
         }
         else{
